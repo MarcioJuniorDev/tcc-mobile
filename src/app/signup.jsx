b8f20c6d3f2c2da813aed5a7867a093591a23289
MarcioJuniorDev <marciojunior2911@gmail.com>
2026-07-24 00:02:25 -0300

    tela de cadastro

---
+++ src/app/signup.jsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import Component_Signup from "../components/signup";
+
+export default function Home() {
+  return (
+    <View style={{ flex: 1 }}>
+      <Component_Signup/>
+    </View>
+  );
+}
